3e3351b35e0bccae13577ba25addaaf4e887bdbd
agfalcon <agfalcon12@gmail.com>
2023-08-10 00:40:00 +0900

    Solve: 1998년생인 내가 태국에서는 2541년생?!
    입출력과 사칙연산

---
+++ src/입출력과사칙연산/B18108.java
@@ -0,0 +1,12 @@
+package 입출력과사칙연산;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class B18108 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine())-543;
+        System.out.println(year);
+    }
+}
