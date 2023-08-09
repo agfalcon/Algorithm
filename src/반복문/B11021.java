5f650b669a4cbe5dbc3a58480deb0b0d8c4d8e4d
agfalcon <agfalcon12@gmail.com>
2023-08-10 08:10:40 +0900

    Solve: A + B - 7
    반복문

---
+++ src/반복문/B11021.java
@@ -0,0 +1,20 @@
+package 반복문;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B11021 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        for(int i=0 ;i<T;i++){
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            sb.append("Case #" + (i+1) + ": " + (n1+n2) + "\n");
+        }
+        System.out.print(sb);
+    }
+}
