3e6927d85ffe6a1ca13bcd7085c30a0dca267839
agfalcon <agfalcon12@gmail.com>
2023-08-10 13:15:48 +0900

    Solve: 과제 안 내신분
    1차원 배열

---
+++ src/_1차원배열/B5587.java
@@ -0,0 +1,16 @@
+package _1차원배열;
+
+import java.io.*;
+public class B5587 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] arr = new boolean[31];
+        for(int i=0;i<28;i++){
+            int num = Integer.parseInt(br.readLine());
+            arr[num] = true;
+        }
+        for(int i=1;i<31;i++){
+            if(!arr[i]) System.out.println(i);
+        }
+    }
+}
