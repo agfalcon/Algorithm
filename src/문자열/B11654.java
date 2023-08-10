6a8df607cd7cee8dc5aa1e20bfe2796c1b27c984
agfalcon <agfalcon12@gmail.com>
2023-08-10 13:49:37 +0900

    Solve: 아스키 코드
    문자열

---
+++ src/문자열/B11654.java
@@ -0,0 +1,11 @@
+package 문자열;
+
+import java.io.*;
+
+public class B11654 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char input = br.readLine().charAt(0);
+        System.out.println((int)input);
+    }
+}
