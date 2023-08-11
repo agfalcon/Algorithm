c7c7d78c2b3580705ffe2f4246f25703939eec28
agfalcon <agfalcon12@gmail.com>
2023-08-11 16:44:02 +0900

    Solve: 대지
    기하: 직사각형과 삼각형

---
+++ src/기하_직사각형과삼각형/B9063.java
@@ -0,0 +1,29 @@
+package 기하_직사각형과삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B9063 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int minX = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        StringTokenizer st;
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if(x<minX) minX = x;
+            if(x>maxX) maxX = x;
+            if(y<minY) minY = y;
+            if(y>maxY) maxY = y;
+        }
+        int area = (maxX-minX) * (maxY-minY);
+        System.out.println(area);
+    }
+}
