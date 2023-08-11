f49844f574c907286bcb5dea8ce4dc340cbe67e9
agfalcon <agfalcon12@gmail.com>
2023-08-11 16:36:32 +0900

    Solve: 수학은 체육과목 입니다.
    기하: 직사각형과 삼각형

---
+++ src/기하_직사각형과삼각형/B15894.java
@@ -0,0 +1,13 @@
+package 기하_직사각형과삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B15894 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n*4);
+    }
+}
