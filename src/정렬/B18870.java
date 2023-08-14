2e5491e90c01d590acf1fcb47675cb2e78c6e87f
agfalcon <agfalcon12@gmail.com>
2023-08-14 16:04:09 +0900

    Solve: 좌표 압축
    정렬

---
+++ src/정렬/B18870.java
@@ -0,0 +1,34 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class B18870 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            set.add(arr[i]);
+        }
+        Integer[] sortedArray = set.toArray(new Integer[0]);
+        Arrays.sort(sortedArray);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<sortedArray.length;i++){
+            map.put(sortedArray[i],i);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<arr.length;i++){
+            sb.append(map.get(arr[i])).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
