98bcb34e1c5673c7b3952b38fedb9aaa7570eda0
agfalcon <agfalcon12@gmail.com>
2023-08-14 16:36:36 +0900

    Solve: 블랙잭
    브루트포스

---
+++ src/브루트포스/B2798.java
@@ -0,0 +1,34 @@
+package 브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B2798 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] card = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            card[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = -1;
+        for(int i=0;i<card.length-2;i++){
+            for(int j=i+1;j<card.length-1;j++){
+                for(int k=j+1;k<card.length;k++){
+                    int sum = card[i] + card[j] + card[k];
+                    if(sum==M){
+                        System.out.println(M);
+                        return;
+                    }
+                    if(sum <= M && answer < sum) answer = sum;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
