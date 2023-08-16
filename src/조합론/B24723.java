facf8ba4fd3977e69960e42b71881cce205652c1
agfalcon <agfalcon12@gmail.com>
2023-08-16 22:14:52 +0900

    Solve: 녹색거탑
    조합론

---
+++ src/조합론/B24723.java
@@ -0,0 +1,13 @@
+package 조합론;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B24723 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((int)Math.pow(2, N));
+    }
+}
