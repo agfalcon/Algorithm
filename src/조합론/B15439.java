6c63bf3f0aa8d868a15f1467a95aab2db5415041
agfalcon <agfalcon12@gmail.com>
2023-08-16 22:11:13 +0900

    Solve: 베라의 패션
    조합론

---
+++ src/조합론/B15439.java
@@ -0,0 +1,13 @@
+package 조합론;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B15439 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N*(N-1));
+    }
+}
