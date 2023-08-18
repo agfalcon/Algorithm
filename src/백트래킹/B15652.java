8e2e8d1dd9452384ec24b5267bb0a4f285d2ea99
agfalcon <agfalcon12@gmail.com>
2023-08-18 17:19:44 +0900

    Solve: N과 M(4)
    백트래킹

---
+++ src/백트래킹/B15652.java
@@ -0,0 +1,36 @@
+package 백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15652 {
+
+    private static int[] answer;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        answer = new int[M];
+        makeSequences(N, 0, M);
+        System.out.print(sb);
+    }
+
+    private static void makeSequences(int N, int level, int depth){
+        if(level==depth){
+            for(int i : answer){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=1;i<=N;i++){
+            if(level!=0 && answer[level-1] > i) continue;
+            answer[level] = i;
+            makeSequences(N, level+1, depth);
+        }
+    }
+}
